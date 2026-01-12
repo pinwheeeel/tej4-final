99846a2cc4f36a5405644d2621ca1bf6222769c5
pinwheeeel <phil.huang@student.tdsb.on.ca>
2026-01-12 13:18:54 -0500

    2

--- final.ino
+++ final.ino
@@ -23,9 +23,17 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 const uint8_t program[] = {
   // bytes array
+  1,2,3,4,5,6,7,8
 };
 
 void setup() {
+  pinMode(CE, OUTPUT);
+  digitalWrite(CE, HIGH);
+  pinMode(OE, OUTPUT);
+  digitalWrite(OE, HIGH);
+  pinMode(WE, OUTPUT);
+  digitalWrite(WE, HIGH);
+
   pinMode(IORQ, INPUT);
   pinMode(MREQ, INPUT);
   pinMode(CLK, OUTPUT);
@@ -38,25 +46,57 @@ void setup() {
   pinMode(M1, INPUT);
   pinMode(RFSH, INPUT);
 
-  pinMode(CE, OUTPUT);
-  pinMode(OE, OUTPUT);
-  pinMode(WE, OUTPUT);
+  Serial.begin(9600);
+  load_program();
+
+  delay(500);
+
+  read_ram();
+
+  Serial.println("Done");
 }
 
 void loop() {
+
 }
 
 void delay_custom() {
   delay(1);
 }
 
+// DEBUG: reading ram
+
+void read_ram() {
+  set_address_bus_output();
+  set_data_bus_input();
+
+  for(int i=0; i<sizeof(program); i++) {
+    write_address_bus(i);
+    // chip enable low
+    digitalWrite(CE, LOW);
+    // orite enable low
+    digitalWrite(OE, LOW);
+    delay_custom();
+
+    Serial.println(read_data_bus());
+    delay_custom();
+
+    // chip enable high
+    digitalWrite(OE, HIGH);
+    // orite enable high
+    digitalWrite(CE, HIGH);
+    delay_custom();
+  }
+}
+
 // LOAD PROGRAM
 
 void load_program() {
+  digitalWrite(CLK, HIGH);
   digitalWrite(RESET, LOW);
   // "RESET must be active for a minimum of three full clock cycles
   // before a reset operation is complete."
-  for (int i = 0; i < 3; i++) {
+  for (int i = 0; i < 4; i++) {
     digitalWrite(CLK, LOW);
     delay_custom();
     digitalWrite(CLK, HIGH);
@@ -66,27 +106,28 @@ void load_program() {
   set_data_bus_output();
   for(int i=0; i<sizeof(program); i++) {
     // write addresses
+    write_address_bus(i);
     // write data
-    
+    write_data_bus(program[i]);
     delay_custom();
 
     // chip enable low
-    // write enable low
     digitalWrite(CE, LOW);
+    // write enable low
     digitalWrite(WE, LOW);
-    delay_custom(); // 190 ns
-
-    // 
+    delay_custom(); // >190 ns
 
     // chip enable high
+    digitalWrite(WE, HIGH);
     // write enable high
+    digitalWrite(CE, HIGH);
+    delay_custom();
   }
 
   set_data_bus_input();
+  set_address_bus_input();
 
-  // busrequest and busack?
-  // digitalWrite(BUSREQ, LOW);f
-  // while(digitalRead(BUSACK == HIGH)); // wait for busack
+
 }
 
 
