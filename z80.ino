1490f93876540a0015e0d8f74ab18f6f5b43645a
pinwheeeel <phil.huang@student.tdsb.on.ca>
2026-01-15 01:09:24 -0500

    cleanup

---
+++ z80.ino
@@ -0,0 +1,223 @@
+// Arduino Mega + Z80 + DS2016
+
+#define IORQ 11
+#define MREQ 12
+#define CLK 13
+#define RD 14
+#define WR 15
+#define BUSACK 16
+#define WAIT 17
+#define BUSREQ 18
+#define RESET 19
+#define M1 20
+#define RFSH 21
+
+#define CE 23
+#define OE 25
+#define WE 27
+
+const int D[8] = { 2, 3, 4, 5, 6, 7, 8, 9 };
+const int A[11] = { 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49 };
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+const uint8_t program[] = {
+  // bytes array
+  0x3E, 0x05,        // ld a,5
+  0x06, 0x03,        // ld b,3
+  0x80,              // add a,b
+  0x32, 0x00, 0x04,  // ld (&0400),a
+  0x76               // halt
+  // 0x18, 0xFE
+};
+
+void setup() {
+  pinMode(CE, OUTPUT);
+  digitalWrite(CE, HIGH);
+  pinMode(OE, OUTPUT);
+  digitalWrite(OE, HIGH);
+  pinMode(WE, OUTPUT);
+  digitalWrite(WE, HIGH);
+
+  pinMode(IORQ, INPUT);
+  pinMode(MREQ, INPUT);
+  pinMode(CLK, OUTPUT);
+  pinMode(RD, INPUT);
+  pinMode(WR, INPUT);
+  pinMode(BUSACK, INPUT);
+  pinMode(WAIT, OUTPUT);
+  pinMode(BUSREQ, OUTPUT);
+  pinMode(RESET, OUTPUT);
+  pinMode(M1, INPUT);
+  pinMode(RFSH, INPUT);
+  Serial.begin(9600);
+
+  digitalWrite(CLK, HIGH);
+  load_program();
+  delay(1000);
+
+  read_ram(0, sizeof(program));
+}
+
+void loop() {
+  digitalWrite(CLK, LOW);
+  delay_custom();
+  digitalWrite(CLK, HIGH);
+  delay_custom();
+
+  // debug information
+  Serial.print(digitalRead(IORQ));
+  Serial.print(" | ");
+  Serial.print(digitalRead(MREQ));
+  Serial.print(" | ");
+  Serial.print(digitalRead(RD));
+  Serial.print(" | ");
+  Serial.println(digitalRead(WR));
+
+  // should this stuff happen when clk is high? low? both?
+  digitalWrite(CE, digitalRead(MREQ));
+  digitalWrite(OE, digitalRead(RD));
+  digitalWrite(WE, digitalRead(WR));
+
+  if (digitalRead(IORQ) == LOW) {  // io request
+    // do something
+  }
+}
+
+void delay_custom() {
+  delay(1);
+}
+
+// DEBUG: reading ram
+void read_ram(int start, int end) {
+  take_bus();
+
+  set_address_bus_output();
+  set_data_bus_input();
+
+  for (int i = start; i < end; i++) {
+    write_address_bus(i);
+    delay_custom();
+    // chip enable low
+    digitalWrite(CE, LOW);
+    // output enable low
+    digitalWrite(OE, LOW);
+    delay_custom();
+
+    Serial.println(read_data_bus());
+    delay_custom();
+
+    // chip enable high
+    digitalWrite(OE, HIGH);
+    // output enable high
+    digitalWrite(CE, HIGH);
+    delay_custom();
+  }
+
+  release_bus();
+}
+
+// LOAD PROGRAM
+void load_program() {
+  digitalWrite(RESET, LOW);
+  // "RESET must be active for a minimum of three full clock cycles
+  // before a reset operation is complete."
+  for (int i = 0; i < 4; i++) {
+    digitalWrite(CLK, LOW);
+    delay_custom();
+    digitalWrite(CLK, HIGH);
+    delay_custom();
+  }
+
+  set_address_bus_output();
+  set_data_bus_output();
+  for (int i = 0; i < sizeof(program); i++) {
+    // write addresses
+    write_address_bus(i);
+    // write data
+    write_data_bus(program[i]);
+    delay_custom();
+
+    // chip enable low
+    digitalWrite(CE, LOW);
+    // write enable low
+    digitalWrite(WE, LOW);
+    delay_custom();  // >190 ns
+
+    // chip enable high
+    digitalWrite(WE, HIGH);
+    // write enable high
+    digitalWrite(CE, HIGH);
+    delay_custom();
+  }
+
+  set_data_bus_input();
+  set_address_bus_input();
+  digitalWrite(RESET, HIGH);
+}
+
+
+// BUS FUNCTIONS
+
+void take_bus() {
+  digitalWrite(BUSREQ, LOW);
+  while (digitalRead(BUSACK) == HIGH);
+}
+
+void release_bus() {
+  digitalWrite(BUSREQ, HIGH);
+  while (digitalRead(BUSACK) == LOW);
+}
+
+// DATA BUS FUNCTIONS
+
+void set_data_bus_input() {
+  for (int i = 0; i < 8; i++)
+    pinMode(D[i], INPUT);
+}
+
+void set_data_bus_output() {
+  for (int i = 0; i < 8; i++)
+    pinMode(D[i], OUTPUT);
+}
+
+int read_data_bus() {
+  int value = 0;
+  for (int i = 0; i < 8; i++) {
+    value |= digitalRead(D[i]) << i;
+  }
+  return value;
+}
+
+void write_data_bus(int value) {
+  for (int i = 0; i < 8; i++) {
+    digitalWrite(D[i], (value >> i) & 1);
+  }
+}
+
+// ADDRESS BUS FUNCTIONS
+
+void set_address_bus_input() {
+  for (int i = 0; i < 11; i++)
+    pinMode(A[i], INPUT);
+}
+
+void set_address_bus_output() {
+  for (int i = 0; i < 11; i++)
+    pinMode(A[i], OUTPUT);
+}
+
+int read_address_bus() {
+  int value = 0;
+  for (int i = 0; i < 11; i++) {
+    value |= digitalRead(A[i]) << i;
+  }
+
+  return value;
+}
+
+void write_address_bus(int value) {
+  for (int i = 0; i < 11; i++) {
+    digitalWrite(A[i], (value >> i) & 1);
+  }
+}
